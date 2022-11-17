da46a174af1989ac5683e9133c918cb30809aaff
madara1319 <mateusz.bik@op.pl>
2022-11-17 20:25:30 +0100

    As for now i do not have idea how to solve this task

--- SecretChamberGame.cpp
+++ SecretChamberGame.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
+#include <vector>
+
+using std::cout,std::endl,std::vector;
 
 int main()
 
 {
-	std::cout<<"Hello world!";
+	vector map(10,10);
+	for (auto m : map)
+		cout <<"+ ";
+	cout<<endl;
+	for (int j{}; j<=10; j++)
+	{
+		for (int i{}; i<=10; i++)
+		{
+			cout<<".";
+		}
+		cout<<endl;
+	}
+
 }
